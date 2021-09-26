0eb7f2304237f62976ecff3fb41968a136931f69
Shivamjha12 <Jhasapna544@gmail.com>
2021-09-26 21:56:26 +0530

    updates

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('API.api.urls')),
-    path('api-auth/', include('rest_framework.urls')),
+    path('user/', include('user_app.api.urls')),
+    # path('api-auth/', include('rest_framework.urls')),
 ]
